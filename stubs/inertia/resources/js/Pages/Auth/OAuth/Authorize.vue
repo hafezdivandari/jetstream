e6679731474a3ad343f31ffa6664a01455dc9d67
Hafez Divandari <hafezdivandari@gmail.com>
2024-08-15 01:33:53 +0330

    wip

--- stubs/inertia/resources/js/Pages/Auth/OAuth/Authorize.vue
+++ stubs/inertia/resources/js/Pages/Auth/OAuth/Authorize.vue
@@ -67,7 +67,7 @@ const deny = () => {
                 </SecondaryButton>
             </form>
 
-            <Link :href="route(route().current(), {...route().params, prompt: 'login'})"
+            <Link :href="route('passport.authorizations.authorize', {...route().params, prompt: 'login'})"
                   class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
                 Log into another account
             </Link>
